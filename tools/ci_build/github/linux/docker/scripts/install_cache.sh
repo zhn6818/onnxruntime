5660c78ab136b51f21f0f420c6629adb42e82859
Yi Zhang <zhanyi@microsoft.com>
2022-04-19 03:24:40 -0700

    use yum

--- tools/ci_build/github/linux/docker/scripts/install_cache.sh
+++ tools/ci_build/github/linux/docker/scripts/install_cache.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+
+#Please make sure libssl-dev is installed.
 set -ex
 
 mkdir -p /opt/cache/bin
 mkdir -p /opt/cache/lib
-
-apt-get -y update
-apt-get install -y libssl1.0.0 libssl-dev
 
 echo "Downloading sccache binary from S3 repo"
 curl --retry 3 https://onnxruntimepackagesint.blob.core.windows.net/bin/sccache -o /opt/cache/bin/sccache
