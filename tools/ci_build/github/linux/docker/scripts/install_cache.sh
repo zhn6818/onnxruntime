b7a759e34d8eda4a24cceb861e6d4fee37fdee1e
Yi Zhang <zhanyi@microsoft.com>
2022-04-19 04:28:46 -0700

    try snap

--- tools/ci_build/github/linux/docker/scripts/install_cache.sh
+++ tools/ci_build/github/linux/docker/scripts/install_cache.sh
@@ -6,10 +6,18 @@ set -ex
 mkdir -p /opt/cache/bin
 mkdir -p /opt/cache/lib
 
-echo "Downloading sccache binary from S3 repo"
-curl --retry 3 https://onnxruntimepackagesint.blob.core.windows.net/bin/sccache -o /opt/cache/bin/sccache
-chmod a+x /opt/cache/bin/sccache
-/opt/cache/bin/sccache --version
+#echo "Downloading sccache binary from S3 repo"
+#curl --retry 3 https://onnxruntimepackagesint.blob.core.windows.net/bin/sccache -o /opt/cache/bin/sccache
+#chmod a+x /opt/cache/bin/sccache
+#/opt/cache/bin/sccache --version
+
+# https://snapcraft.io/install/sccache/centos
+yum install -y epel-release
+yum install -y snapd
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
+snap install -y sccache --candidate --classic
+sccache --version
 
 function write_sccache_stub() {
   printf "#!/bin/sh\nif [ \$(ps -p \$PPID -o comm=) != sccache ]; then\n  exec sccache $(which $1) \"\$@\"\nelse\n  exec $(which $1) \"\$@\"\nfi" > "/opt/cache/bin/$1"
