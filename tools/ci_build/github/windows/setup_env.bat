864f4a45156a5aed4e3123aad2f537008d512da3
Changming Sun <chasun@microsoft.com>
2022-05-10 18:13:03 -0700

    update

--- tools/ci_build/github/windows/setup_env.bat
+++ tools/ci_build/github/windows/setup_env.bat
@@ -1,2 +1,6 @@
+REM Copyright (c) Microsoft Corporation. All rights reserved.
+REM Licensed under the MIT License.
+
+REM This file is used by "DML Nuget Pipeline", "Nuget WindowsAI Pipeline", "Zip-Nuget-Java-Nodejs Packaging Pipeline", "Windows CPU CI Pipeline", "Windows GPU CI Pipeline".
 set PATH=C:\azcopy;%PATH%
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
--- tools/ci_build/github/windows/setup_env_cuda_11.bat
+++ tools/ci_build/github/windows/setup_env_cuda_11.bat
@@ -1,2 +1,6 @@
+REM Copyright (c) Microsoft Corporation. All rights reserved.
+REM Licensed under the MIT License.
+
+REM This file is used by "DML Nuget Pipeline", "Zip-Nuget-Java-Nodejs Packaging Pipeline", "Windows GPU CI Pipeline".
 set PATH=C:\azcopy;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.1\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.4\extras\CUPTI\lib64;%PATH%
 set GRADLE_OPTS=-Dorg.gradle.daemon=false
