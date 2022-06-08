38c065574a1004828e199081f4d6fe7b99034373
Changming Sun <chasun@microsoft.com>
2022-06-07 23:37:14 -0700

    update

--- cmake/wil.cmake
+++ cmake/wil.cmake
@@ -2,13 +2,12 @@
 # Licensed under the MIT License.
 add_library(wil INTERFACE)
 
- if(onnxruntime_USE_SUBMODULE)
-       target_include_directories(wil INTERFACE external/wil/include/)
-    else()
+if(onnxruntime_USE_SUBMODULE)
+    target_include_directories(wil INTERFACE external/wil/include)
+else()
     FetchContent_Declare(
       microsoft_wil
-      GIT_REPOSITORY https://github.com/microsoft/wil.git
-      GIT_TAG        e8c599bca6c56c44b6730ad93f6abbc9ecd60fc1
+      URL https://github.com/microsoft/wil/archive/e8c599bca6c56c44b6730ad93f6abbc9ecd60fc1.zip
     )
     FetchContent_Populate(microsoft_wil)
     target_include_directories(wil INTERFACE ${microsoft_wil_SOURCE_DIR}/include)
