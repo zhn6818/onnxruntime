7cd3df25657eda452f40a0c50e74ea9a7dde645d
Guangyun Han <guangyunhan@microsoft.com>
2022-08-17 01:59:19 -0700

    Update ck to latest master and enable interwave scheduling

--- cmake/external/composable_kernel.cmake
+++ cmake/external/composable_kernel.cmake
@@ -1,5 +1,5 @@
 set(composable_kernel_URL https://github.com/ROCmSoftwarePlatform/composable_kernel.git)
-set(composable_kernel_TAG 85978e0201bb94bf6e59b325e1f5f19266845d08) # 2022-07-22 11:52:10 -0700
+set(composable_kernel_TAG c961ce9226dd263af1d898c02c0afae0ed702f7d) # 2022-08-16 01:04:20 +0800
 
 set(BUILD_DEV OFF)
 set(PATCH ${PROJECT_SOURCE_DIR}/patches/composable_kernel/Fix_Clang_Build.patch)
